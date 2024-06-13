c91f7688603dd9839cba9ac9a825a9da78691de7
Anthony Sofio <anthonysofio@microsoft.com>
2024-06-13 17:44:35 -0500

    Added functionality to validate app settings upon app startup.

--- reference-implementations/semantic-search-for-images/src/ingestion/Startup.cs
+++ reference-implementations/semantic-search-for-images/src/ingestion/Startup.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Azure.Core;
 using Azure.Identity;
 using Azure.Storage.Blobs;
@@ -18,8 +19,13 @@ public class Startup
     {
         Configuration = new ConfigurationBuilder()
                             .SetBasePath(Directory.GetCurrentDirectory())
-                            .AddJsonFile("appsettings.json", optional: false)
+                            .AddJsonFile("appsettings.json", optional: true)
+                            .AddEnvironmentVariables()
                             .Build();
+
+        var appSettings = Configuration.GetSection("AppSettings").Get<AppSettings>();
+
+        ValidateAppSettings(appSettings!);
     }
 
     public void ConfigureServices(IServiceCollection services)
@@ -52,5 +58,55 @@ public class Startup
                 })
                 .AddTimeout(TimeSpan.FromSeconds(10));
         });
+    }
+
+    private void ValidateAppSettings(AppSettings appSettings) {
+        if (appSettings == null)
+            throw new Exception("AppSettings not found via appsettings.json or environment variables.");
+
+        var settingsMissing = false;
+        var sb = new StringBuilder();
+        sb.AppendLine("The following app settings were not found in appsettings.json or an environment variable and are missing:");
+
+        if (string.IsNullOrEmpty(appSettings.StorageAccountUri) ||
+            string.IsNullOrEmpty(appSettings.StorageAccountImageCsvContainerName) ||
+            string.IsNullOrEmpty(appSettings.StorageAccountProcessedContainerName) ||
+            string.IsNullOrEmpty(appSettings.CosmosDbUri) ||
+            string.IsNullOrEmpty(appSettings.CosmosDbName) ||
+            string.IsNullOrEmpty(appSettings.CosmosDbImageVectorPath) ||
+            string.IsNullOrEmpty(appSettings.CosmosDbImageMetadataContainerName) ||
+            string.IsNullOrEmpty(appSettings.CosmosDbPartitionkey) ||
+            string.IsNullOrEmpty(appSettings.AiServicesUri) ||
+            string.IsNullOrEmpty(appSettings.AiServicesApiVersion) ||
+            string.IsNullOrEmpty(appSettings.AiServicesModelVersion))
+        {
+            settingsMissing = true;
+        }
+
+        if (string.IsNullOrEmpty(appSettings.StorageAccountUri))
+            sb.AppendLine("StorageAccountUri not found.");
+        if (string.IsNullOrEmpty(appSettings.StorageAccountImageCsvContainerName))
+            sb.AppendLine("StorageAccountImageCsvContainerName not found.");
+        if (string.IsNullOrEmpty(appSettings.StorageAccountProcessedContainerName))
+            sb.AppendLine("StorageAccountProcessedContainerName not found.");
+        if (string.IsNullOrEmpty(appSettings.CosmosDbUri))
+            sb.AppendLine("CosmosDbUri not found.");
+        if (string.IsNullOrEmpty(appSettings.CosmosDbName))
+            sb.AppendLine("CosmosDbName not found.");
+        if (string.IsNullOrEmpty(appSettings.CosmosDbImageVectorPath))
+            sb.AppendLine("CosmosDbImageVectorPath not found.");
+        if (string.IsNullOrEmpty(appSettings.CosmosDbImageMetadataContainerName))
+            sb.AppendLine("CosmosDbImageMetadataContainerName not found.");
+        if (string.IsNullOrEmpty(appSettings.CosmosDbPartitionkey))
+            sb.AppendLine("CosmosDbPartitionkey not found.");
+        if (string.IsNullOrEmpty(appSettings.AiServicesUri))
+            sb.AppendLine("AiServicesUri not found.");
+        if (string.IsNullOrEmpty(appSettings.AiServicesApiVersion))
+            sb.AppendLine("AiServicesApiVersion not found.");
+        if (string.IsNullOrEmpty(appSettings.AiServicesModelVersion))
+            sb.AppendLine("AiServicesModelVersion not found.");
+        
+        if(settingsMissing)
+            throw new Exception(sb.ToString());
     }
 }
